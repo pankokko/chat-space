0a2d2fd9b1a01898211095ce29c4e591d270b209
pankokko <teshi41@gmail.com>
2019-10-29 17:51:47 +0900

    メッセージ送信機能完了　＊画像送信不可

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ devise_for :users
 root 'groups#index'
     resources :users, only: [:edit, :update]
     resources :groups, only: [:new, :create, :edit, :update] do
-      resources :messages, only: [:index]
+      resources :messages, only: [:index,:create]
    end
   end
