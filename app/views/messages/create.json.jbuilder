ed89af404f170117e2943a4032d25d33684bd795
pankokko <teshi41@gmail.com>
2019-11-02 22:08:05 +0900

    11月2日 22時版

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,5 @@
-json.content @message.content
-json.image @message.image
+json.(@message, :content, :image)
 json.created_at @message.created_at
 json.user_name @message.user.name
+#idもデータとして渡す
+json.id @message.id
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ root 'groups#index'
     resources :users, only: [:index,:edit, :update]
     resources :groups, only: [:new, :create, :edit, :update] do
       resources :messages, only: [:index,:create]
+
+      namespace :api do
+        resources :messages, only: :index, defaults: { format: 'json' }
+      end
    end
   end
